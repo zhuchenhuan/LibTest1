51a70495ea64a09e15eddd4e64a0e22167a171ad
lee lee <656785862@qq.com>
2017-06-01 17:15:41 +0800

    添加项目文件。

---
+++ LibTest1/LibTest1.h
@@ -0,0 +1,32 @@
+
+// LibTest1.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CLibTest1App: 
+// 有关此类的实现，请参阅 LibTest1.cpp
+//
+
+class CLibTest1App : public CWinApp
+{
+public:
+	CLibTest1App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CLibTest1App theApp;
